0ae1011c46426965733fc4b782364ce930806d4c
Lokep <iamlokep@163.com>
2018-11-29 18:05:54 +0800

    without config

---
+++ QiDeLong/src/components/footer/footer.vue
@@ -0,0 +1,68 @@
+<style>
+	.footer{
+		position: fixed;
+		left: 0;
+		right: 0;
+		margin: auto;
+		bottom: 0;
+		z-index: 10;
+		background-color: #fff;
+		height: 1.05rem;
+	}
+	.footer-row{
+		width: 7.2rem;
+		margin: 0 auto;
+		height: 1.05rem;
+	}
+	.footer .footer-link{
+		line-height: 1.15rem;
+		font-size: .6rem;
+		color: #c2c2c2;
+	}
+	.footer .router-link-active{
+		color: #000;
+	}
+	.footer i{
+		color: inherit;
+		font-size: inherit;
+		line-height: inherit;
+	}
+</style>
+<template>
+	<div class="footer">
+		<van-row class="footer-row">
+		  	<van-col span="6">
+		  		<router-link class="footer-link" to='/'>
+		  			<i class="fa fa-home"></i>
+		  		</router-link>
+		  	</van-col>
+		  	<van-col span="6">
+		  		<router-link class="footer-link" to='/movie'>
+		  			<i class="fa fa-camera" style="font-size: .5rem"></i>
+		  		</router-link>
+		  	</van-col>
+		  	<van-col span="6">
+		  		<router-link class="footer-link" to='social'>
+		  			<i class="fa fa-compass"></i>
+		  		</router-link>	
+		  		
+		  	</van-col>
+		  	<van-col span="6">
+		  		<router-link class="footer-link" to='/user'>
+		  			<i class="fa fa-user"></i>
+		  		</router-link>	
+		  		
+		  	</van-col>
+		</van-row>
+	</div>
+</template>
+<script>
+	export default{
+		data(){
+			return{
+
+			}
+		},
+
+	}
+</script>
--- QiDeLong/src/views/Home.vue
+++ QiDeLong/src/views/Home.vue
@@ -6,7 +6,6 @@
 		width:6.7rem;
 		height: 1.05rem;
 		margin: 0  auto;
-		padding-top: .3rem;
 	}
 	.home-title,.home-sub-title{
 		vertical-align: bottom;
@@ -20,24 +19,101 @@
 		font-size: .28rem;
 		color: #ababab;
 	}
+	.home-search{
+		width: 6.4rem;
+		margin: 0 auto;
+		font-size: .25rem;
+		border-bottom: 1px solid #b6b6b6;
+	}
+	.home-search>input{
+		height: .4rem;
+		width: 5rem;
+		padding: .2rem 0;
+	}
+	.home-search-icon{
+		margin:.3rem .2rem 0 0;
+	}
+	.home-swiper{
+		height: 10.35rem;
+	}
+	.swiper-item{
+		width: 100%;
+	}
+	.swiper-item>div{
+		width: 6.125rem;
+		margin: .5rem auto .5rem;
+	    -o-box-shadow: 0.5px 0.866px 35px 0 rgba(0, 0, 0, 0.21);
+	    -webkit-box-shadow: 0.5px 0.866px 35px 0 rgba(0, 0, 0, 0.21);
+	    box-shadow: 0.5px 0.866px 35px 0 rgba(0, 0, 0, 0.21);
+	    border-radius: 4px;
+	    overflow: hidden;
+	}
+	.swiper-item img{
+		width: 100%;
+	}
+	.swiper-item p{
+		font-size: .28rem;
+		margin: .4rem 0 0 .2rem;
+		text-align: left;
+	}
+	.swiper-item span{
+		text-align: right;
+		font-size: .3rem;
+		margin-right: .2rem;
+		margin: .2rem .2rem .2rem 0;
+	}
 </style>
 <template>
 	<div class="home">
 		<div class="home-head ov">
 			<h4 class="home-title fl">轻影</h4>
 			<span class="home-sub-title fr">深夜记得早点睡</span>
-			<router-link to='./Search.vue' >
-				
-			</router-link>	
+
 		</div>
+		<div class="home-search ov">
+			<input class="fl" placeholder="请输入影片" @focus="homeRedirect">
+			<van-icon name="search" class="fr home-search-icon"/>
+		</div>	
+		<div class="home-swiper">
+			<van-swipe class="swiper">
+				<van-swipe-item class="swiper-item">
+					<div>
+						<img src="../assets/images/home-banner.jpg" alt="movie">
+						<p>当你凝望深渊的时候，深渊也在凝望你</p>
+						<span>-----尼采</span>
+					</div>
+				</van-swipe-item>
+				<van-swipe-item class="swiper-item">
+					<div>
+						<img src="../assets/images/home-banner.jpg" alt="movie">
+						<p>当你凝望深渊的时候，深渊也在凝望你</p>
+						<span>-----尼采</span>
+					</div>
+				</van-swipe-item>
+				<van-swipe-item class="swiper-item">
+					<div>
+						<img src="../assets/images/home-banner.jpg" alt="movie">
+						<p>当你凝望深渊的时候，深渊也在凝望你</p>
+						<span>-----尼采</span>
+					</div>
+				</van-swipe-item>
+			</van-swipe>
+		</div>
+		<Footer></Footer>	
 	</div>
 </template>
 <script>
+
+	import Footer from '../components/footer/footer.vue'
+
 	export default {
 		data(){
 			return{
 
 			}
+		},
+		components:{
+			Footer
 		},
 		metaInfo: {
 			title:'lokep',
@@ -47,6 +123,15 @@
 					content:'white'
 				}
 			]
+		},
+		methods:{
+			homeRedirect(){
+				let path = this.$router.path
+				if(path=='/'){
+					this.$router.push('/Search')
+				}
+
+			}
 		}
 	}
 </script>
