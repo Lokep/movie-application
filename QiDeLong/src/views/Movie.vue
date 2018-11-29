de24c1a4423e9bfd214b8ad456a0a5f3285d8948
Lokep <iamlokep@163.com>
2018-11-29 09:23:17 +0800

    add COMPONENTS
    添加components，.gitkeep为占位文件

--- QiDeLong/src/views/Movie.vue
+++ QiDeLong/src/views/Movie.vue
@@ -1,45 +1,59 @@
 <template>
-  <div class="movie">
-    <van-nav-bar>
-      <template slot="left">
-        <span class="page-title">影片大全</span>
-      </template>
-      <template slot="right">
-        <van-icon class="search-icon" size=".36rem" color="#1a1f25" name="search" />
-        <van-icon size=".36rem" color="#1a1f25" name="wap-nav" />
-      </template>
-    </van-nav-bar>
-    <van-swipe @change="onChange">
-      <van-swipe-item v-for="(image, index) in images" :key="index">
+    <div class="movie">
+        <van-nav-bar>
+            <template slot="left">
+                <span class="page-title">影片大全</span>
+            </template>
+            <template slot="right">
+                <van-icon class="search-icon" size=".36rem" color="#1a1f25" name="search" />
+                <van-icon size=".36rem" color="#1a1f25" name="wap-nav" />
+            </template>
+        </van-nav-bar>
+        <van-swipe @change="onChange">
+            <!-- <van-swipe-item v-for="(image, index) in images" :key="index">
         <img src="item" alt="">
-      </van-swipe-item>
-      <div class="custom-indicator" slot="indicator">
-        {{ current + 1 }}/4
-      </div>
-    </van-swipe>
-  </div>
+      </van-swipe-item> -->
+            <van-swipe-item>
+                <img class="banner" src="../assets/images/banner.jpg" alt="">
+            </van-swipe-item>
+            <van-swipe-item>
+                <img class="banner" src="../assets/images/banner.jpg" alt="">
+            </van-swipe-item>
+            <van-swipe-item>
+                <img class="banner" src="../assets/images/banner.jpg" alt="">
+            </van-swipe-item>
+            <div class="custom-indicator" slot="indicator">
+                {{ current + 1 }}/4
+            </div>
+        </van-swipe>
+    </div>
 </template>
 <script>
-  export default {
+export default {
     data() {
-      return {
-        current: 0
-      }
+        return {
+            current: 0,
+        }
     },
     methods: {
-      onChange(index) {
-        this.current = index;
-      }
+        onChange(index) {
+            this.current = index;
+        }
     }
-  }
-</script>
+}
 
+</script>
 <style scoped>
 .page-title {
-  color: #1a1f25;
-  font-size: .36rem !important;
+    color: #1a1f25;
+    font-size: .36rem !important;
 }
+
 .search-icon {
-  margin-right: .2rem;
+    margin-right: .2rem;
 }
+.banner{
+    width: 100%;
+}
+
 </style>
