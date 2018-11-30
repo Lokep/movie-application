d6d1376a309ce2a161407f9461ac27c5d28c3239
Lokep <iamlokep@163.com>
2018-11-30 15:32:57 +0800

    node

--- QiDeLong/config/index.js
+++ QiDeLong/config/index.js
@@ -23,9 +23,9 @@ module.exports = {
 
     // Various Dev Server settings
     //host: 'localhost', // can be overwritten by process.env.HOST
-    host: '192.168.191.1',
+    host: '192.168.1.90',
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-    autoOpenBrowser: true,
+    autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
