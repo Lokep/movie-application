dfb1608a272c46ac640abfeddbd9cfde872d4f61
unknown <weblixin@163.com>
2018-12-07 14:54:36 +0800

    社区页面

---
+++ QiDeLong/src/components/social/socialCityGrid.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="social-city-grid">
+    <div class="head-img">
+      <img :src="cityInfo.img" alt="">
+    </div>
+    <div class="city-user-info">
+      <img class="avatar" :src="cityInfo.avatar" alt="">
+      <p class="content">{{cityInfo.content}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    cityInfo: Object
+  }
+}
+</script>
+
+<style scoped>
+.social-city-grid {
+  width: 46%;
+  margin-right: 4%;
+  margin-bottom: .3rem;
+  float: left;
+}
+.head-img {
+  width: 100%;
+  height: 3.6rem;
+  overflow: hidden;
+}
+.head-img img {
+  width: 100%;
+}
+.city-user-info {
+  padding: .2rem .1rem;
+  border: 1px solid #eee;
+  overflow: hidden;
+}
+.avatar {
+  width: .8rem;
+  float: left;
+  vertical-align: middle;
+  margin-right: .1rem;
+}
+.content {
+  font-size: .24rem;
+  margin-top: .05rem;
+}
+</style>
--- QiDeLong/src/views/social/socialCity.vue
+++ QiDeLong/src/views/social/socialCity.vue
@@ -1,5 +1,52 @@
 <template>
   <div class="social-city">
-    同城轻友
+    <social-city-grid v-for="(item, index) in cityData" :key="index" :cityInfo="item"></social-city-grid>
   </div>
 </template>
+
+<script>
+import socialCityGrid from '@/components/social/socialCityGrid'
+export default {
+  components: {
+    socialCityGrid
+  },
+  data() {
+    return {
+      cityData: [
+        {
+          img: 'https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2541280047.jpg',
+          avatar: '../../../static/images/dynamic/1.png',
+          content: '今天晚上吃什么今天晚上吃什么'
+        }, {
+          img: 'https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2541197052.jpg',
+          avatar: '../../../static/images/dynamic/2.png',
+          content: '今天晚上吃什么今天晚上吃什么'
+        }, {
+          img: 'https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2540474819.jpg',
+          avatar: '../../../static/images/dynamic/3.png',
+          content: '今天晚上吃什么今天晚上吃什么'
+        }, {
+          img: 'https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2539661066.jpg',
+          avatar: '../../../static/images/dynamic/4.png',
+          content: '今天晚上吃什么今天晚上吃什么'
+        }, {
+          img: 'https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2537667301.jpg',
+          avatar: '../../../static/images/dynamic/5.png',
+          content: '今天晚上吃什么今天晚上吃什么'
+        }, {
+          img: 'https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2541280047.jpg',
+          avatar: '../../../static/images/dynamic/1.png',
+          content: '今天晚上吃什么今天晚上吃什么'
+        }, 
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.social-city {
+  padding: .3rem;
+  margin-right: -4%;
+}
+</style>
